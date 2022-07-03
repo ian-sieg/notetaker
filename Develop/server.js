66948297741b5b6871d6d15ec0b8e610c3faa456
ian.moss <ianmsieg@gmail.com>
2022-07-02 21:15:47 -0500

    fixing up server with proper routing and api requests, still have to add fs read and write

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const path = require('path')
-// const api = require('./routes/index.js')
-const notes = require('./db/db.json')
-const PORT = process.env.PORT || 3001;
-const app = express()
+const path = require('path');
+const fs = require('fs');
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+const notesDb = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -17,12 +22,29 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'db/db.json'))
+    res.status(200).json(notesDb)
 );
 
-app.post('/api/notes', (req, res) =>
-    res.json(notes)
-);
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body
+    // fs.readFileSync('./db/db.json')
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid()
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in creating new note')
+    };
+});
 
 app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/index.html'))
